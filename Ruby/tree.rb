55294fd258d2d7a11f92c93822b419f548f42013
Philip Vegdahl <pvegdahl@gmail.com>
2021-07-20 15:46:52 -0400

    Do the day 2 Ruby HW.

--- Ruby/tree.rb
+++ Ruby/tree.rb
@@ -6,22 +6,35 @@ class Tree
     @node_name = name
   end
 
-  def visit_all(&block)
-    visit(&block)
-    children.each {|c| c.visit_all &block}
+  def self.construct_from_hash(hash)
+    return self.construct_from_hash_helper(hash)[0]
   end
 
-  def visit(&block)
-    block.call self
+  def self.construct_from_hash_helper(hash)
+    hash.map {|k, v| Tree.new(k, self.construct_from_hash_helper(v))}
+  end
+
+  def visit_all(depth, &block)
+    visit(depth, &block)
+    children.each {|c| c.visit_all(depth+1, &block)}
+  end
+
+  def visit(depth, &block)
+    block.call(self, depth)
   end
 end
 
 ruby_tree = Tree.new("Ruby", [Tree.new("Reia"), Tree.new("MacRuby")])
 
 puts "Visiting a node"
-ruby_tree.visit {|node| puts node.node_name}
+ruby_tree.visit(0) {|node, depth| puts ".." * depth + node.node_name}
 puts
 
-
 puts "Visiting the whole tree"
-ruby_tree.visit_all {|node| puts node.node_name}
+ruby_tree.visit_all(0) {|node, depth| puts ".." * depth + node.node_name}
+
+family_hash = {"grandpa" => {"dad" => {"child1" => {}, "child2" => {}}, "uncle" => {"cousin" => {}}}}
+family_tree = Tree.construct_from_hash(family_hash)
+
+puts "Visiting the family tree"
+family_tree.visit_all(0) {|node, depth| puts ".." * depth + node.node_name}

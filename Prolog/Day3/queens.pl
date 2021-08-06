3c13524381b45c0f7b462420fd2255e5f2be7775
Philip Vegdahl <pvegdahl@gmail.com>
2021-08-06 09:46:26 -0400

    Queesns is working hard coded to 4

--- Prolog/Day3/queens.pl
+++ Prolog/Day3/queens.pl
@@ -6,24 +6,33 @@ columns([(_, Column)|QueensTail], [Column|ColumnsTail]) :- columns(QueensTail, C
 
 diagonals1([], []).
 diagonals1([(Row, Column)|QueensTail], [Diagonal|DiagonalTail]) :-
-    Diagonal is Column - Row,
+    Diagonal is (Column - Row),
     diagonals1(QueensTail, DiagonalTail).
 
 diagonals2([], []).
 diagonals2([(Row, Column)|QueensTail], [Diagonal|DiagonalTail]) :-
-    Diagonal is Column + Row,
+    Diagonal is (Column + Row),
     diagonals2(QueensTail, DiagonalTail).
+
+valid_queen((Row, Column)) :-
+    Range = [1, 2, 3, 4],
+    member(Row, Range),
+    member(Column, Range).
+
+valid_board([]).
+valid_board([Head|Tail]) :- valid_queen(Head), valid_board(Tail).
     
-queens(Size, Solution) :-
-    length(Solution, Size),
-    rows(Solution, Rows),
-    columns(Solution, Columns),
-    diagonals1(Solution, Diagonals1),
-    diagonals2(Solution, Diagonals2),
-    fd_domain(Rows, 1, Size),
-    fd_domain(Columns, 1, Size),
+queens(Board) :-
+    Size is 4,
+    length(Board, Size),
+    valid_board(Board),
+
+    rows(Board, Rows),
+    columns(Board, Columns),
+    diagonals1(Board, Diagonals1),
+    diagonals2(Board, Diagonals2),
+
     fd_all_different(Rows),
     fd_all_different(Columns),
-    fd_all_different(Diagonals1).
+    fd_all_different(Diagonals1),
     fd_all_different(Diagonals2).
-    

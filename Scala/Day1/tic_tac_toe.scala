97f38080c9d7e74252e9e3df3ed513e53a32bade
Philip Vegdahl <pvegdahl@gmail.com>
2021-08-11 16:35:51 -0400

    Remove some unnecessary return statements

--- Scala/Day1/tic_tac_toe.scala
+++ Scala/Day1/tic_tac_toe.scala
@@ -7,30 +7,30 @@ def get_winner(board: Array[Array[Char]]): Char = {
   get_winner(get_diagonal_1(board)).foreach(winner => return winner)
   get_winner(get_diagonal_2(board)).foreach(winner => return winner)
 
-  return '?'
+  '?'
 }
 
 def get_column(board: Array[Array[Char]], i: Int): Array[Char] = {
-  return Array(board(i)(0), board(i)(1), board(i)(2))
+  Array(board(i)(0), board(i)(1), board(i)(2))
 }
 
 def get_row(board: Array[Array[Char]], i: Int): Array[Char] = {
-  return Array(board(0)(i), board(1)(i), board(2)(i))
+  Array(board(0)(i), board(1)(i), board(2)(i))
 }
 
 def get_diagonal_1(board: Array[Array[Char]]): Array[Char] = {
-  return Array(board(0)(0), board(1)(1), board(2)(2))
+  Array(board(0)(0), board(1)(1), board(2)(2))
 }
 
 def get_diagonal_2(board: Array[Array[Char]]): Array[Char] = {
-  return Array(board(0)(2), board(1)(1), board(2)(0))
+  Array(board(0)(2), board(1)(1), board(2)(0))
 }
 
 def get_winner(triple: Array[Char]): Option[Char] = {
   if (triple(0) == triple(1) && triple(0) == triple(2)) {
-    return Some(triple(0))
+    Some(triple(0))
   } else {
-    return None
+    None
   }
 }
 

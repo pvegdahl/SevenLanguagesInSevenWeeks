1389df6f61f175cf64e30fde712a3086b1053f1d
Philip Vegdahl <pvegdahl@gmail.com>
2022-04-14 14:32:52 -0400

    Did N Queens in Haskell for practice / fun

--- Haskell/day1/homework.hs
+++ Haskell/day1/homework.hs
@@ -14,7 +14,21 @@ module Main where
 
     timesTables :: (Integer) -> [(Integer, Integer, Integer)]
     timesTables n = [(a, b, a*b) | a <- [0..n], b <- [0..n]]
-    -- different
+
+    nQueens :: (Integer) -> [Integer]
+    nQueens n = nQueensRecursive([], n)
+
+    nQueensRecursive :: ([Integer], Integer) -> [Integer]
+    nQueensRecursive (board, n)
+        | n == toInteger(length(board)) = board
+        | otherwise = headOrEmptyList(filter (/=[]) [nQueensRecursive(board ++ [newPos], n) | newPos <- [0..n-1], checkNewQueen(board, newPos)])
+
+    headOrEmptyList :: [[Integer]] -> [Integer]
+    headOrEmptyList x = if x == [] then [] else head x
+
+    checkNewQueen :: ([Integer], Integer) -> Bool
+    checkNewQueen ([], n) = True
+    checkNewQueen ((h:t), n) = (h /= n) && (toInteger(1 + length t) /= abs(h - n)) && checkNewQueen(t, n)
 
     main :: IO ()
     main = return ()

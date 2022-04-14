ff603bd2b9f2fad5acb2cb1d1cd9294592e50ead
Philip Vegdahl <pvegdahl@gmail.com>
2022-04-14 10:18:16 -0400

    My good friend Fibonacci

---
+++ Haskell/day1/fib.hs
@@ -0,0 +1,15 @@
+module Main where
+
+    fib :: Integer -> Integer
+    fib x = fibResult(fibTuple(0, 1, x))
+
+    fibTuple :: (Integer, Integer, Integer) -> (Integer, Integer, Integer)
+    fibTuple (x, y, 0) = (x, y, 0)
+    fibTuple (x, y, index) = fibTuple(y, x+y, index-1)
+
+    fibResult :: (Integer, Integer, Integer) -> Integer
+    fibResult (x, _, _) = x
+
+    main :: IO ()
+    main = return ()
+
